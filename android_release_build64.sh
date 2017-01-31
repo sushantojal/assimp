c3699537e781478b1ae885532001b2dd580aed81
nola.donato@samsung.com <nola.donato@samsung.com>
2017-01-30 18:30:22 -0800

    adding new android build scripts, removing the old ones
    New build scripts are android_debug_build32.sh, android_release_build32.sh, android_debug_build64.sh and android_release_build64.sh

---
+++ android_release_build64.sh
@@ -0,0 +1,2 @@
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=android-19 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=gnustl_static -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=/home/nola.donato/android-sdks/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=gcc
+
