3b348047fb38daf71e45c3dc5f76428a936fa737
Tom Flynn <tom.flynn@samsung.com>
2018-04-30 19:03:13 -0700

    use c++_shared standard runtime
    use c++_shared standard runtime

--- android_debug_build32.sh
+++ android_debug_build32.sh
@@ -1,3 +1,4 @@
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=gnustl_static -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=/home/nola.donato/android-sdks/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=gcc
+NDK_BUNDLE ?= /home/flynnt/trees/android-sdk/ndk-bundle
+cmake --trace -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=c++_shared -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=$(NDK_BUNDLE)/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang
 make
 
--- android_release_build32.sh
+++ android_release_build32.sh
@@ -1,4 +1,5 @@
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=gnustl_static -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=/home/nola.donato/android-sdks/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=gcc
+NDK_BUNDLE ?= /home/flynnt/trees/android-sdk/ndk-bundle
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=c++_shared -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=$(NDK_BUNDLE)/build/cmake/android.toolchain.cmake  -DANDROID_TOOLCHAIN=clang
 make
 cd lib
 cp libassimp.so libassimp.so.debug
