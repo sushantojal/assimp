7ad9f4c0555d470dd5cf0632663b3dc2a02912e5
Mihail Marinov <m.marinov@samsung.com>
2018-05-29 13:09:19 -0700

    Build with ndk api level 22

--- android_release_build64.sh
+++ android_release_build64.sh
@@ -1,4 +1,4 @@
-cmake -G "Unix Makefiles" -DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=android-24 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=c++_shared -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=$(NDK_BUNDLE)/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang
+cmake -G "Unix Makefiles" -DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=android-22 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=c++_shared -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=$NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang
 make -j12
 cd lib
 cp libassimp.so libassimp.so.debug
