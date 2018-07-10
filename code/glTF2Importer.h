5621f88be3cc98473c6b731231e4709a7e0f8844
Sushant Ojal <sojal@cs.stonybrook.edu>
2018-07-09 18:55:04 -0700

    morph meshes + initial animation

--- code/glTF2Importer.h
+++ code/glTF2Importer.h
@@ -83,6 +83,7 @@ private:
     void ImportCameras(glTF2::Asset& a);
     void ImportLights(glTF2::Asset& a);
     void ImportNodes(glTF2::Asset& a);
+    void ImportAnimations(glTF2::Asset& a);
 
 };
 
