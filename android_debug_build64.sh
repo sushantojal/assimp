951fc66d508cf3afd45deb408327c4486f2cc75d
Nola Donato <nola.donato@samsung.com>
2018-12-06 16:41:25 -0800

    updated assimp build scripts

--- android_debug_build64.sh
+++ android_debug_build64.sh
@@ -1,3 +1,16 @@
-cmake --trace -G "Unix Makefiles" -DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=android-22 -DANDROID_FORCE_ARM_BUILD=TRUE -DCMAKE_INSTALL_PREFIX=install -DANDROID_STL=c++_shared -DANDROID_STL_FORCE_FEATURES=ON -DCMAKE_TOOLCHAIN_FILE=$NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN=clang
-make -j12
+ANDROID_SDK=~/Android/Sdk
+NDKROOT=$ANDROID_SDK/ndk-bundle
+$ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
+	-DCMAKE_MAKE_PROGRAM=$NDKROOT/prebuilt/linux-x86_64/bin/make \
+	-DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a  \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DANDROID_ABI=arm64-v8a \
+	-DANDROID_NATIVE_API_LEVEL=android-22 \
+	-DANDROID_FORCE_ARM_BUILD=TRUE \
+	-DCMAKE_INSTALL_PREFIX=install \
+	-DANDROID_STL=c++_shared \
+	-DANDROID_STL_FORCE_FEATURES=ON \
+	-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake \
+	-DANDROID_TOOLCHAIN=clang
+$NDKROOT/prebuilt/linux-x86_64/bin/make -j12
 
