90ed7f0e7d9d2be29564b3667bff5c2f0a6ed24e
Nola Donato <nola.donato@samsung.com>
2018-12-10 19:15:09 -0800

    Check for null data pointers, Intel build scripts

--- intel_debug_build64.sh
+++ intel_debug_build64.sh
@@ -3,8 +3,8 @@ NDKROOT=$ANDROID_SDK/ndk-bundle
 $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DCMAKE_MAKE_PROGRAM=$NDKROOT/prebuilt/linux-x86_64/bin/make \
 	-DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a  \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DANDROID_ABI=arm64-v8a \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DANDROID_ABI=x86_64 \
 	-DANDROID_NATIVE_API_LEVEL=android-22 \
 	-DANDROID_FORCE_ARM_BUILD=TRUE \
 	-DCMAKE_INSTALL_PREFIX=install \
@@ -13,5 +13,4 @@ $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake \
 	-DANDROID_TOOLCHAIN=clang
 $NDKROOT/prebuilt/linux-x86_64/bin/make -j12
-$NDKROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin/strip lib/libassimp.so
 
--- intel_release_build32.sh
+++ intel_release_build32.sh
@@ -4,7 +4,7 @@ $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DCMAKE_MAKE_PROGRAM=$NDKROOT/prebuilt/linux-x86_64/bin/make \
 	-DANDROID_LINKER_FLAGS=-Wl,--exclude-libs,libunwind.a  \
 	-DCMAKE_BUILD_TYPE=Release \
-	-DANDROID_ABI=arm64-v8a \
+	-DANDROID_ABI=x86 \
 	-DANDROID_NATIVE_API_LEVEL=android-22 \
 	-DANDROID_FORCE_ARM_BUILD=TRUE \
 	-DCMAKE_INSTALL_PREFIX=install \
@@ -13,5 +13,4 @@ $ANDROID_SDK/cmake/3.6.4111459/bin/cmake --trace -G "Unix Makefiles" \
 	-DCMAKE_TOOLCHAIN_FILE=$NDKROOT/build/cmake/android.toolchain.cmake \
 	-DANDROID_TOOLCHAIN=clang
 $NDKROOT/prebuilt/linux-x86_64/bin/make -j12
-$NDKROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin/strip lib/libassimp.so
 
